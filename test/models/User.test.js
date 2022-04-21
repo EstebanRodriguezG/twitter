1b06aad3d52cd7848a108e993c4c74f5e078b34b
Esteban Rodriguez <erodriguezg7@gmail.com>
2022-04-20 23:45:02 -0500

    Agregando prueba jest para User

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,7 +1,14 @@
+const User = require("../../app/models/User")
+
 describe("Prueba de JEST", ()=>{
 
     test("prueba 1", ()=>{
 
         expect(10).toBe(10);
+    })
+
+    test("prueba 2", () =>{
+        const user = new User(1,"erodriguez", "Esteban", "bio", new Date());
+        expect(user.name).toBe("Esteban");
     })
 })
